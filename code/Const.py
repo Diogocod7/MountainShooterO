845fa90377955cf1ffcb52c856a9c19275d1637d
diogo <diogocode30@gmail.com>
2026-03-04 15:54:58 -0300

    FIXING IMPORTS IN lEVEL.PY FILE

--- code/Const.py
+++ code/Const.py
@@ -1,8 +1,11 @@
-# C
 
+# C
+import pygame
 
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
+COLOR_GREEN = (0, 128, 0)
+COLOR_CYAN = (0, 128, 128)
 
 # M
 MENU_OPTION = ("NEW GAME 1P",
@@ -10,6 +13,13 @@ MENU_OPTION = ("NEW GAME 1P",
                 "NEW GAME 3P - COOPERATIVE",
                 "SCORE",
                 "EXIT")
+
+# S
+SPAWN_TIME = 4000
+
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 # W
 WIN_WIDTH = 576
@@ -19,6 +29,8 @@ WIN_HEIGHT = 324
 COLOR_YELLOW = (255, 255, 0)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -27,5 +39,6 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+
 
 }
